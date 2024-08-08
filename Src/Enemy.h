6f47846f41cd03c2a33a5fccc3e2c18076066821
敬子 善原 <2316032@s.asojuku.ac.jp>
2024-08-08 10:48:06 +0900

    Enemyクラス追加。

---
+++ Src/Enemy.h
@@ -0,0 +1,19 @@
+#pragma once
+class Enemy
+{
+public:
+	// 画像サイズ
+	static constexpr int SCREEN_SIZE_X = 1024;
+	static constexpr int SCREEN_SIZE_Y = 640;
+
+	// アニメーション数
+	// IDLE
+	static constexpr int IDLE_ANIM_NUM = 4;
+
+	static constexpr int RUN_ANIM_NUM = 7;
+
+private:
+
+
+};
+
